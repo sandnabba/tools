b78bd9ff8a7c2174e80970f40ffb6b5becb0375a
Emil@DvorakMobile <Emil@DvorakMobile>
2017-02-26 17:31:55 +0100

    Fully working version

--- clean_snapshots.sh
+++ clean_snapshots.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-# This is a generic script for managing backups in the format:
+# This is a generic script for managing btrfs snapshots in the format:
 # /path/$NAME_PREFIX-$YEAR-$MONTH-$DAY
 # By default it will keep daily snapshots for 30 days, weekly snapshots for 3 months
 # and monthly snapshots for 1 year.
 
-NAME_PREFIX=test
-SNAPSHOT_DIR=/home/emil/temp/snapshots
-DELETE_COMMAND="rmdir"
 
-TODAY="2017-02-02"
-DAY=2
-CURRENT_MONTH=2
-YEAR=2017
+if [ $# -eq 0 ]; then
+  echo "No arguments supplied"
+  exit 1
+else
+  NAME_PREFIX="$1"
+fi
 
-# TODAY=$(date +20%y-%m-%d)
-# DAY=$(date +%d)
-# CURRENT_MONTH=$(date +%m)
-#YEAR=$(date +20%y)
+SNAPSHOT_DIR="/btrfs/snapshots"
+DELETE_COMMAND="btrfs subvolume delete"
+
+# Debug:
+#TODAY="2017-03-01"
+#DAY=2
+#CURRENT_MONTH=3
+#YEAR=2017
+
+TODAY=$(date +20%y-%m-%d)
+DAY=$(date +%d)
+CURRENT_MONTH=$(date +%m)
+YEAR=$(date +20%y)
 
 if [ $CURRENT_MONTH -eq 1 ]; then
   LAST_MONTH=12
@@ -40,12 +48,11 @@ if [ "$TWO_MONTHS_AGO" -le "9" ]; then
   TWO_MONTHS_AGO="0$TWO_MONTHS_AGO"
 fi
 
-echo "$SNAPSHOT_DIR"/"$NAME_PREFIX"_"$TODAY"
-
 # Stop if we don't have a snapshot for today:
-if [ ! -d "$SNAPSHOT_DIR"/"$NAME_PREFIX"_"$TODAY" ];
+if [ ! -d "$SNAPSHOT_DIR"/"$NAME_PREFIX""$TODAY" ];
 then
-  echo "CRITICAL: Today does not exist!"
+  echo "CRITICAL: Snapshot for today does not exist!"
+  echo "$SNAPSHOT_DIR"/"$NAME_PREFIX""$TODAY"
   exit 2
 fi
 
@@ -58,14 +65,14 @@ if [ $DAY -eq 1 ]; then
     if [ $CURRENT_MONTH -eq 2 ]; then
       YEAR=$(expr $YEAR - 1)
     fi
-    echo checking "$SNAPSHOT_DIR"/"$NAME_PREFIX"_"$YEAR"-"$TWO_MONTHS_AGO"-"$DAY"
-    if [ -d "$SNAPSHOT_DIR"/"$NAME_PREFIX"_"$YEAR"-"$TWO_MONTHS_AGO"-"$DAY" ]; then
-      echo Removing "$SNAPSHOT_DIR"/"$NAME_PREFIX"_"$YEAR"-"$TWO_MONTHS_AGO"-"$DAY"
-      $($DELETE_COMMAND "$SNAPSHOT_DIR"/"$NAME_PREFIX"_"$YEAR"-"$TWO_MONTHS_AGO"-"$DAY")
+    #echo checking "$SNAPSHOT_DIR"/"$NAME_PREFIX""$YEAR"-"$TWO_MONTHS_AGO"-"$DAY"
+    if [ -d "$SNAPSHOT_DIR"/"$NAME_PREFIX""$YEAR"-"$TWO_MONTHS_AGO"-"$DAY" ]; then
+      #echo Removing "$SNAPSHOT_DIR"/"$NAME_PREFIX""$YEAR"-"$TWO_MONTHS_AGO"-"$DAY"
+      $($DELETE_COMMAND "$SNAPSHOT_DIR"/"$NAME_PREFIX""$YEAR"-"$TWO_MONTHS_AGO"-"$DAY")
     fi
   done
 else
   # Just delete last month's snapshot
-  echo "Removing "$SNAPSHOT_DIR"/"$NAME_PREFIX"_"$YEAR"-"$LAST_MONTH"-"$DAY""
-  $($DELETE_COMMAND "$SNAPSHOT_DIR"/"$NAME_PREFIX"_"$YEAR"-"$LAST_MONTH"-"$DAY")
+  #echo "Removing "$SNAPSHOT_DIR"/"$NAME_PREFIX""$YEAR"-"$LAST_MONTH"-"$DAY""
+  $($DELETE_COMMAND "$SNAPSHOT_DIR"/"$NAME_PREFIX""$YEAR"-"$LAST_MONTH"-"$DAY")
 fi
